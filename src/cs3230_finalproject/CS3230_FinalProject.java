3bee9fa43ced12cf076e70674d2b277ee29e1eac
Brendan Ralston <ralston.brendan@me.com>
2018-07-17 22:34:55 -0600

    Fixed typo in project, created splash screen transition

---
+++ src/cs3230_finalproject/CS3230_FinalProject.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs3230_finalproject;
+
+import javafx.animation.PauseTransition;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+/**
+ *
+ * @author ralst
+ */
+public class CS3230_FinalProject extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+	Parent splashScreenRoot = FXMLLoader.load(getClass().getResource("splashScreen.fxml"));
+
+	Scene splashScreenScene = new Scene(splashScreenRoot);
+
+	stage.setScene(splashScreenScene);
+	stage.show();
+
+	Parent mainMenuParent = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+	Scene scene = new Scene(mainMenuParent);
+
+	PauseTransition splashScreenDisplayDelay = new PauseTransition(Duration.seconds(2));
+	splashScreenDisplayDelay.setOnFinished(new EventHandler<ActionEvent>() {
+	    @Override
+	    public void handle(ActionEvent event)
+	    {
+		stage.setScene(scene);
+		stage.show();
+	    }
+	});
+	splashScreenDisplayDelay.play();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+	launch(args);
+    }
+
+}
---
+++ src/cs3230_finalproject/MainMenuController.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs3230_finalproject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author ralst
+ */
+public class MainMenuController implements Initializable {
+
+    @FXML
+    private Button warButton;
+    
+    @FXML
+    private Button sequenceButton;
+    
+    @FXML
+    private void handleWarButton(ActionEvent event) throws IOException {
+        System.out.println("Lets play war!!");
+        Parent screen2Parent = FXMLLoader.load(getClass().getResource("warDescription.fxml"));
+        Scene scene = new Scene(screen2Parent);
+        
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    @FXML
+    private void handleSequenceButton(ActionEvent event) throws IOException {
+        System.out.println("Lets play war!!");
+        Parent screen2Parent = FXMLLoader.load(getClass().getResource("sequenceDescription.fxml"));
+        Scene scene = new Scene(screen2Parent);
+        
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+	// TODO
+    }    
+    
+}
