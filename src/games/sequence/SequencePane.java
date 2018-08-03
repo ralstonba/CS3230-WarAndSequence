949c2f0c4945ed280344a1518f28f48c8f68f33d
Brendan Ralston <ralston.brendan@me.com>
2018-08-02 23:00:47 -0600

    Add cards to pane and center, made function to fade in board after shuffling

--- src/games/sequence/SequencePane.java
+++ src/games/sequence/SequencePane.java
@@ -1,7 +1,9 @@
 package games.sequence;
 
+import javafx.animation.FadeTransition;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 /**
  *
@@ -13,14 +15,29 @@ public class SequencePane extends StackPane {
     private Player bluePlayer;
     private Player greenPlayer;
     private Deck deck;
+    private boolean bluePlayerTurn;
 
-    public void initilize()
-    {
-	boardLayout = new BoardPane();
+    public void initilize() {
+        boardLayout = new BoardPane();
         bluePlayer = new Player(PieceType.BLUE);
         greenPlayer = new Player(PieceType.GREEN);
-	
-	//boardLayout.relocate(getWidth()/2, getHeight()/2);
-	getChildren().add(boardLayout);
+        deck = new Deck();
+
+        for (Card c : deck.cards) {
+            getChildren().add(c);
+            moveCard(c, getWidth() / 2, getHeight() / 2);
+        }
+    }
+
+    private void moveCard(Card c, double x, double y) {
+        c.relocate(x - c.getCenterX(), y - c.getCenterY());
+    }
+    
+    private void showBoard(){
+        getChildren().add(boardLayout);
+        FadeTransition ft = new FadeTransition(Duration.millis(500), boardLayout);
+        ft.setFromValue(0);
+        ft.setToValue(1);
+        ft.play();
     }
 }
